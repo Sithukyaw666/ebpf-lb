5ea3b26671fca47297e147484733031146f87f79
Sithukyaw666 <sithukyaw27500@gmail.com>
2026-04-13 16:51:23 +0000

    added go mod

--- lb.c
+++ lb.c
@@ -2,18 +2,16 @@
 #include "vmlinux.h"
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
-#include "parse_helpers.h"
 
+#include "parse_helpers.h"
 #define NUM_BACKENDS 2
 #define ETH_ALEN 6
 #define AF_INET 2
 
-//upstream_server_ips
+// upstream_server_ips
 struct endpoint {
   __u32 ip;
 };
-
-
 
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -36,243 +34,259 @@ struct {
   __type(value, __u32);
 } rr_state SEC(".maps");
 
-//return next backend pointer
-//without loop and direct lookup 
+// return next backend pointer
+// without loop and direct lookup
 static __always_inline struct endpoint *get_next_backend() {
   __u32 key = 0;
   __u32 *last_idx = bpf_map_lookup_elem(&rr_state, &key);
-  if (!last_idx) return NULL;
+  if (!last_idx)
+    return NULL;
 
-  __u32 current_idx = __sync_fetch_and_add(last_idx, 1);
+  __sync_fetch_and_add(last_idx, 1);
 
-  __u32 target_idx = current_idx % NUM_BACKENDS;
+  __u32 target_idx = (*last_idx) % NUM_BACKENDS;
 
   return bpf_map_lookup_elem(&backends, &target_idx);
 }
-// check the error type at https://github.com/torvalds/linux/blob/e774d5f1bc27a85f858bce7688509e866f8e8a4e/include/uapi/linux/bpf.h#L7300
+// check the error type at
+// https://github.com/torvalds/linux/blob/e774d5f1bc27a85f858bce7688509e866f8e8a4e/include/uapi/linux/bpf.h#L7300
 
 static __always_inline void log_fib_error(int rc) {
-  switch(rc) {
-	  case BPF_FIB_LKUP_RET_BLACKHOLE:
-		  bpf_printk("FIB lookup failed: BLACKHOLE route. Check 'ip r'.");
-		  break;
-          case BPF_FIB_LKUP_RET_UNREACHABLE:
-		  bpf_printk("FIB lookup failed: UNREACHABLE route.");
-		  break;
-	  case BPF_FIB_LKUP_RET_PROHIBIT: 
-		  bpf_printk("FIB lookup failed: PROHIBITED route.");
-	          break;
-          case BPF_FIB_LKUP_RET_NOT_FWDED:
-	  	  bpf_printk("FIB lookup failed: NOT_FORWARDED route. Destination might be on same subnet");
-		  break;
-          case BPF_FIB_LKUP_RET_FWD_DISABLED:
-	  	  bpf_printk("FIB lookup failed: FORWARDING_DISABLED. Enable via sysctl");
-		  break;
-	  case BPF_FIB_LKUP_RET_UNSUPP_LWT:
-	    	  bpf_printk("FIB lookup failed: UNSUPPORTED_PACKET. fwd requires encapsulation");
-		  break;
-          case BPF_FIB_LKUP_RET_NO_NEIGH:
-	          bpf_printk("FIB lookup failed: NO NEIGHBOUR FOUND. Populate the cache with ping");
-		  break;
-	  case BPF_FIB_LKUP_RET_FRAG_NEEDED:
-	  	  bpf_printk("FIB lookup failed: FRAGMENTATION NEEDED. Packet exceed MTU");
-		  break;
-	  case 	BPF_FIB_LKUP_RET_NO_SRC_ADDR:
-	  	  bpf_printk("FIB lookup failed: NO SOURCE ADDR FOUND. Make sure source interface has legit ip");
-		  break;
-          default:
-		  bpf_printk("FIB lookup failed: rc=%d (unknown). Check routing and ARP/NDP config", rc);
-		  break;
+  switch (rc) {
+  case BPF_FIB_LKUP_RET_BLACKHOLE:
+    bpf_printk("FIB lookup failed: BLACKHOLE route. Check 'ip r'.");
+    break;
+  case BPF_FIB_LKUP_RET_UNREACHABLE:
+    bpf_printk("FIB lookup failed: UNREACHABLE route.");
+    break;
+  case BPF_FIB_LKUP_RET_PROHIBIT:
+    bpf_printk("FIB lookup failed: PROHIBITED route.");
+    break;
+  case BPF_FIB_LKUP_RET_NOT_FWDED:
+    bpf_printk("FIB lookup failed: NOT_FORWARDED route. Destination might be "
+               "on same subnet");
+    break;
+  case BPF_FIB_LKUP_RET_FWD_DISABLED:
+    bpf_printk("FIB lookup failed: FORWARDING_DISABLED. Enable via sysctl");
+    break;
+  case BPF_FIB_LKUP_RET_UNSUPP_LWT:
+    bpf_printk(
+        "FIB lookup failed: UNSUPPORTED_PACKET. fwd requires encapsulation");
+    break;
+  case BPF_FIB_LKUP_RET_NO_NEIGH:
+    bpf_printk(
+        "FIB lookup failed: NO NEIGHBOUR FOUND. Populate the cache with ping");
+    break;
+  case BPF_FIB_LKUP_RET_FRAG_NEEDED:
+    bpf_printk("FIB lookup failed: FRAGMENTATION NEEDED. Packet exceed MTU");
+    break;
+  case BPF_FIB_LKUP_RET_NO_SRC_ADDR:
+    bpf_printk("FIB lookup failed: NO SOURCE ADDR FOUND. Make sure source "
+               "interface has legit ip");
+    break;
+  default:
+    bpf_printk(
+        "FIB lookup failed: rc=%d (unknown). Check routing and ARP/NDP config",
+        rc);
+    break;
   }
 }
-//see the bpf_fib_lookup struct in https://github.com/libbpf/libbpf/blob/dd92bef7f6c7a00bb0312554119b6d9cf38e4f32/include/uapi/linux/bpf.h#L7315
-//see the xdp_md struct in https://github.com/libbpf/libbpf/blob/dd92bef7f6c7a00bb0312554119b6d9cf38e4f32/include/uapi/linux/bpf.h#L6559
-static __always_inline int fib_lookup_v4_full(struct xdp_md *ctx, struct bpf_fib_lookup *fib, __u32 src, __u32 dst, __u16 tot_len) {
-	//zero and populate only what full lookup need
-	__builtin_memset(fib, 0, sizeof(*fib));
-	//hardcode the family for ipv4 only
-	fib->family = AF_INET;
-	//src addr of the packet
-        fib->ipv4_src = src;
-	//dst addr of the packet
-        fib->ipv4_dst = dst;	
-	//hardcode the protocol for tcp only 	
-	fib->l4_protocol = IPPROTO_TCP;
-	//total length of the packet (header + payload)
-	fib->tot_len = tot_len;
-	//ingress interface for the lookup
-	fib->ifindex = ctx->ingress_ifindex;
-        
-	return bpf_fib_lookup(ctx, fib, sizeof(*fib), 0);
+// see the bpf_fib_lookup struct in
+// https://github.com/libbpf/libbpf/blob/dd92bef7f6c7a00bb0312554119b6d9cf38e4f32/include/uapi/linux/bpf.h#L7315
+// see the xdp_md struct in
+// https://github.com/libbpf/libbpf/blob/dd92bef7f6c7a00bb0312554119b6d9cf38e4f32/include/uapi/linux/bpf.h#L6559
+static __always_inline int fib_lookup_v4_full(struct xdp_md *ctx,
+                                              struct bpf_fib_lookup *fib,
+                                              __u32 src, __u32 dst,
+                                              __u16 tot_len) {
+  // zero and populate only what full lookup need
+  __builtin_memset(fib, 0, sizeof(*fib));
+  // hardcode the family for ipv4 only
+  fib->family = AF_INET;
+  // src addr of the packet
+  fib->ipv4_src = src;
+  // dst addr of the packet
+  fib->ipv4_dst = dst;
+  // hardcode the protocol for tcp only
+  fib->l4_protocol = IPPROTO_TCP;
+  // total length of the packet (header + payload)
+  fib->tot_len = tot_len;
+  // ingress interface for the lookup
+  fib->ifindex = ctx->ingress_ifindex;
+
+  return bpf_fib_lookup(ctx, fib, sizeof(*fib), 0);
 }
-// calculate the checksum 
+// calculate the checksum
 // checksum 16 bit
 // helper funtion return 64 bit
 // need to fold (keep the last 16 bit and add carry)
 // 4 loop since 16*4 64 (maximum number to loop)
 static __always_inline __u16 recalc_ip_checksum(struct iphdr *ip) {
-  //clear checksum for recalculation 
+  // clear checksum for recalculation
   ip->check = 0;
 
-  __u64 csum = bpf_csum_diff(NULL, 0, (unsigned int *)ip, sizeof(struct iphdr), 0);
+  __u64 csum =
+      bpf_csum_diff(NULL, 0, (unsigned int *)ip, sizeof(struct iphdr), 0);
 
 #pragma unroll
   for (int i = 0; i < 4; i++) {
-	  if (csum >> 16)
-		  csum = (csum & 0xffff) + (csum >> 16);
+    if (csum >> 16)
+      csum = (csum & 0xffff) + (csum >> 16);
   }
   return ~csum;
 }
-	
+
 SEC("xdp")
 int xdp_loadbalancer(struct xdp_md *ctx) {
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct hdr_cursor nh;
-	nh.pos = data;
+  void *data_end = (void *)(long)ctx->data_end;
+  void *data = (void *)(long)ctx->data;
+  struct hdr_cursor nh;
+  nh.pos = data;
 
-	//parse the eth headers
-	struct ethhdr *eth;
-	int eth_type = parse_ethhdr(&nh, data_end, &eth);
+  // parse the eth headers
+  struct ethhdr *eth;
+  int eth_type = parse_ethhdr(&nh, data_end, &eth);
 
-	/*	
-       	 * only lb ipv4 (ETH_P_IP), for all or ipv4 (ETH_P_ALL or ETH_P_IPV6)
-	 * bpf_htons -> change the host byte order(based on host endian) to network byte order (big endian)	
-	 * comparing value inside network packet with the host constant
-	 */
-	if (eth_type != bpf_htons(ETH_P_IP)) {
-		return XDP_PASS;
-	}
-	
-	//parse the ip headers
-	struct iphdr *ip;
-	parse_iphdr(&nh, data_end, &ip);
-	//if no data -> pass
-	if ((void *)(ip + 1) > data_end) {
-		return XDP_PASS;
-	}
+  /*
+   * only lb ipv4 (ETH_P_IP), for all or ipv4 (ETH_P_ALL or ETH_P_IPV6)
+   * bpf_htons -> change the host byte order(based on host endian) to network
+   * byte order (big endian) comparing value inside network packet with the host
+   * constant
+   */
+  if (eth_type != bpf_htons(ETH_P_IP)) {
+    return XDP_PASS;
+  }
 
-	//only for tcp 
-	if (ip->protocol != IPPROTO_TCP) {
-		return XDP_PASS;
-	}
+  // parse the ip headers
+  struct iphdr *ip;
+  parse_iphdr(&nh, data_end, &ip);
+  // if no data -> pass
+  if ((void *)(ip + 1) > data_end) {
+    return XDP_PASS;
+  }
 
-	//parse tcp headers
-	struct tcphdr *tcp;
-	parse_tcphdr(&nh, data_end, &tcp);
-	if ((void *)(tcp + 1) > data_end) {
-		return XDP_PASS;
-	}
+  // only for tcp
+  if (ip->protocol != IPPROTO_TCP) {
+    return XDP_PASS;
+  }
 
-        //hardcode the port number for simplicity
- 	
-	if (tcp->dest != bpf_htons(8000)) {
-		return XDP_PASS;
-	}
-	
-	//roundrobin 
-	struct endpoint *backend =  get_next_backend();
+  // parse tcp headers
+  struct tcphdr *tcp;
+  parse_tcphdr(&nh, data_end, &tcp);
+  if ((void *)(tcp + 1) > data_end) {
+    return XDP_PASS;
+  }
 
-	if (!backend) {
-		return XDP_ABORTED;
-	}
+  // hardcode the port number for simplicity
 
-	struct bpf_fib_lookup fib = {};
+  if (tcp->dest != bpf_htons(8000)) {
+    return XDP_PASS;
+  }
 
-	int rc = fib_lookup_v4_full(ctx, &fib, ip->daddr, backend->ip, bpf_ntohs(ip->tot_len));
+  // roundrobin
+  struct endpoint *backend = get_next_backend();
 
-	if (rc != BPF_FIB_LKUP_RET_SUCCESS) {
-		log_fib_error(rc);
-		return XDP_ABORTED;
-	}
+  if (!backend) {
+    return XDP_ABORTED;
+  }
 
-	/*
-	 * we are using ipip encapsulation for DSR layer 3
-	 * simply rewriting the src and dst work but lb become bottleneck for handling both request and response
-	 * client -> lb -> backend, backend -> lb -> client
-	 * making upstream server return directly to client is ideal approach for high performance
-	 * can only rewrite the dest ip and pass to backend 
-	 * but client will drop response packet if req packet dst and res packet src ip are different
-	 * without rewriting the dest or src ip but mac addr rewriting , need virtual ip to share for both lb and backends
-	 * but backends and lb must be in the same layer 2 network
-	 * use ipip encapsulation for lb and backend can exist in different layer 2 network
-	*/
+  struct bpf_fib_lookup fib = {};
 
-	/*
-	 * make room for new outer ipv4 header (20bytes) between eth and inner ipv4 headers
-	 * use negative delta for expending , since data-pointer move left , leaving more room
-	*/
-	int adj = bpf_xdp_adjust_head(ctx, 0 - (int)sizeof(struct iphdr));
-	if (adj < 0) {
-		bpf_printk("Failed to adjust packet header");
-		return XDP_ABORTED;
-	}
+  int rc = fib_lookup_v4_full(ctx, &fib, ip->daddr, backend->ip,
+                              bpf_ntohs(ip->tot_len));
 
-	// recompute data pointers after adjusting the headers
-	void *new_data_end = (void *)(long)ctx->data_end;
-	void *new_data = (void *)(long)ctx->data;
+  if (rc != BPF_FIB_LKUP_RET_SUCCESS) {
+    log_fib_error(rc);
+    return XDP_ABORTED;
+  }
 
-	struct ethhdr *new_eth = new_data;
-	if ((void *)(new_eth + 1) > new_data_end) {
-		return XDP_ABORTED;
-	}
-	
-	// outer ipv4 header lives right after ethernet
-	struct iphdr *outer = (void *)(new_eth + 1);
-	if ((void *)(outer + 1) > new_data_end) {
-		return XDP_ABORTED;
-	}
+  /*
+   * we are using ipip encapsulation for DSR layer 3
+   * simply rewriting the src and dst work but lb become bottleneck for handling
+   * both request and response client -> lb -> backend, backend -> lb -> client
+   * making upstream server return directly to client is ideal approach for high
+   * performance can only rewrite the dest ip and pass to backend but client
+   * will drop response packet if req packet dst and res packet src ip are
+   * different without rewriting the dest or src ip but mac addr rewriting ,
+   * need virtual ip to share for both lb and backends but backends and lb must
+   * be in the same layer 2 network use ipip encapsulation for lb and backend
+   * can exist in different layer 2 network
+   */
 
-	// inner ipv4 header lives right after outer
-	struct iphdr *inner = (void *)(outer + 1);
-	if ((void *)(inner + 1) > new_data_end) {
-		return XDP_ABORTED;
-	}
+  /*
+   * make room for new outer ipv4 header (20bytes) between eth and inner ipv4
+   * headers use negative delta for expending , since data-pointer move left ,
+   * leaving more room
+   */
+  int adj = bpf_xdp_adjust_head(ctx, 0 - (int)sizeof(struct iphdr));
+  if (adj < 0) {
+    bpf_printk("Failed to adjust packet header");
+    return XDP_ABORTED;
+  }
 
-	//update the packet eth header with backend mac addr from the arp table
-	__builtin_memcpy(new_eth->h_source, fib.smac, ETH_ALEN);
-	__builtin_memcpy(new_eth->h_dest, fib.dmac, ETH_ALEN);
-	new_eth->h_proto = bpf_htons(ETH_P_IP); //ipv4
-	
+  // recompute data pointers after adjusting the headers
+  void *new_data_end = (void *)(long)ctx->data_end;
+  void *new_data = (void *)(long)ctx->data;
 
-	//build outer ipv4 header
-	__u16 inner_len = bpf_ntohs(inner->tot_len);
-	__u16 outer_len = (__u16)(inner_len + sizeof(struct iphdr));
+  struct ethhdr *new_eth = new_data;
+  if ((void *)(new_eth + 1) > new_data_end) {
+    return XDP_ABORTED;
+  }
 
-	outer->version = 4; //ipv4
-	outer->ihl = 5; //20 bytes 
- 	outer->tos = 0; //(type of service) best effort 		
-	outer->tot_len = bpf_htons(outer_len);
-	outer->id = 0;
-	outer->frag_off = 0; //packet pagination offset
-	outer->ttl = 64;
-	outer->protocol = IPPROTO_IPIP; //for kernel ipip module to check and proceed decapsulation
-	
+  // outer ipv4 header lives right after ethernet
+  struct iphdr *outer = (void *)(new_eth + 1);
+  if ((void *)(outer + 1) > new_data_end) {
+    return XDP_ABORTED;
+  }
 
-	__u32 lbkey = 0;
-	struct endpoint *lb = bpf_map_lookup_elem(&load_balancer, &lbkey);
-       	if (!lb) {
-		return XDP_ABORTED;
-	}
-	//this is only for routing the encapsulated packet
-	outer->saddr = lb->ip; 
-	outer->daddr = backend->ip; 
-	outer->check = 0; // initialise
+  // inner ipv4 header lives right after outer
+  struct iphdr *inner = (void *)(outer + 1);
+  if ((void *)(inner + 1) > new_data_end) {
+    return XDP_ABORTED;
+  }
 
-	/*
-	 * need to recalculate the layer3 checksum since we build the outer ip header from scratch
-	 * since we are operating layer below kernel network stack,
-	 * every changes in the packet headers need to recalculate the checksum 
-	*/
-	outer->check = recalc_ip_checksum(outer);
-	
-	/*
-	 * we don't need to recalculate the ethernet frame checksum after updating the mac address
-	 * bacause ethernet frame checksum (FCS) isn't in the header 
-	 * but instead is automatically recomputed by the NIC hardware when packet is transmitted
-	*/
-	return XDP_TX;
-}	
+  // update the packet eth header with backend mac addr from the arp table
+  __builtin_memcpy(new_eth->h_source, fib.smac, ETH_ALEN);
+  __builtin_memcpy(new_eth->h_dest, fib.dmac, ETH_ALEN);
+  new_eth->h_proto = bpf_htons(ETH_P_IP); // ipv4
+
+  // build outer ipv4 header
+  __u16 inner_len = bpf_ntohs(inner->tot_len);
+  __u16 outer_len = (__u16)(inner_len + sizeof(struct iphdr));
+
+  outer->version = 4; // ipv4
+  outer->ihl = 5;     // 20 bytes
+  outer->tos = 0;     //(type of service) best effort
+  outer->tot_len = bpf_htons(outer_len);
+  outer->id = 0;
+  outer->frag_off = 0; // packet pagination offset
+  outer->ttl = 64;
+  outer->protocol =
+      IPPROTO_IPIP; // for kernel ipip module to check and proceed decapsulation
+
+  __u32 lbkey = 0;
+  struct endpoint *lb = bpf_map_lookup_elem(&load_balancer, &lbkey);
+  if (!lb) {
+    return XDP_ABORTED;
+  }
+  // this is only for routing the encapsulated packet
+  outer->saddr = lb->ip;
+  outer->daddr = backend->ip;
+  outer->check = 0; // initialise
+
+  /*
+   * need to recalculate the layer3 checksum since we build the outer ip header
+   * from scratch since we are operating layer below kernel network stack, every
+   * changes in the packet headers need to recalculate the checksum
+   */
+  outer->check = recalc_ip_checksum(outer);
+
+  /*
+   * we don't need to recalculate the ethernet frame checksum after updating the
+   * mac address bacause ethernet frame checksum (FCS) isn't in the header but
+   * instead is automatically recomputed by the NIC hardware when packet is
+   * transmitted
+   */
+  return XDP_TX;
+}
 char _license[] SEC("license") = "GPL";
-
-
--- parse_helpers.h
+++ parse_helpers.h
@@ -1,57 +1,27 @@
 /* SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-clause) */
-/*
- * This file contains parsing functions that are used in the packetXX XDP
- * programs. The functions are marked as __always_inline, and fully defined in
- * this header file to be included in the BPF program.
- *
- * Each helper parses a packet header, including doing bounds checking, and
- * returns the type of its contents if successful, and -1 otherwise.
- *
- * For Ethernet and IP headers, the content type is the type of the payload
- * (h_proto for Ethernet, nexthdr for IPv6), for ICMP it is the ICMP type field.
- * All return values are in host byte order.
- *
- * The versions of the functions included here are slightly expanded versions of
- * the functions in the packet01 lesson. For instance, the Ethernet header
- * parsing has support for parsing VLAN tags.
- */
+/* This file was modified but originally taken from
+ * https://github.com/xdp-project/xdp-tutorial */
 
-#ifndef __PARSING_HELPERS_H
-#define __PARSING_HELPERS_H
+#ifndef __PARSE_HELPERS_H
+#define __PARSE_HELPERS_H
 
-#include <stddef.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <linux/ip.h>
-#include <linux/ipv6.h>
-#include <linux/icmp.h>
-#include <linux/icmpv6.h>
-#include <linux/udp.h>
-#include <linux/tcp.h>
+#define ETH_P_8021Q 0x8100
+#define ETH_P_8021AD 0x88A8
+#define ETH_P_IP 0x0800
+#define ETH_P_IPV6 0x86DD
+#define ICMP_ECHOREPLY 0
+#define ICMP_ECHO 8
+#define IPPROTO_ICMPV6 58
+#define ICMPV6_ECHO_REQUEST 128
+#define ICMPV6_ECHO_REPLY 129
+
+#ifndef XDP_ACTION_MAX
+#define XDP_ACTION_MAX (XDP_REDIRECT + 1)
+#endif
 
 /* Header cursor to keep track of current parsing position */
 struct hdr_cursor {
-	void *pos;
-};
-
-/*
- *	struct vlan_hdr - vlan header
- *	@h_vlan_TCI: priority and VLAN ID
- *	@h_vlan_encapsulated_proto: packet type ID or len
- */
-struct vlan_hdr {
-	__be16	h_vlan_TCI;
-	__be16	h_vlan_encapsulated_proto;
-};
-
-/*
- * Struct icmphdr_common represents the common part of the icmphdr and icmp6hdr
- * structures.
- */
-struct icmphdr_common {
-	__u8		type;
-	__u8		code;
-	__sum16	cksum;
+  void *pos;
 };
 
 /* Allow users of header file to redefine VLAN max depth */
@@ -59,16 +29,15 @@ struct icmphdr_common {
 #define VLAN_MAX_DEPTH 2
 #endif
 
-#define VLAN_VID_MASK		0x0fff /* VLAN Identifier */
+#define VLAN_VID_MASK 0x0fff /* VLAN Identifier */
 /* Struct for collecting VLANs after parsing via parse_ethhdr_vlan */
 struct collect_vlans {
-	__u16 id[VLAN_MAX_DEPTH];
+  __u16 id[VLAN_MAX_DEPTH];
 };
 
-static __always_inline int proto_is_vlan(__u16 h_proto)
-{
-	return !!(h_proto == bpf_htons(ETH_P_8021Q) ||
-		  h_proto == bpf_htons(ETH_P_8021AD));
+static __always_inline int proto_is_vlan(__u16 h_proto) {
+  return !!(h_proto == bpf_htons(ETH_P_8021Q) ||
+            h_proto == bpf_htons(ETH_P_8021AD));
 }
 
 /* Notice, parse_ethhdr() will skip VLAN tags, by advancing nh->pos and returns
@@ -77,196 +46,155 @@ static __always_inline int proto_is_vlan(__u16 h_proto)
  * VLAN tagged packet.
  */
 static __always_inline int parse_ethhdr_vlan(struct hdr_cursor *nh,
-					     void *data_end,
-					     struct ethhdr **ethhdr,
-					     struct collect_vlans *vlans)
-{
-	struct ethhdr *eth = nh->pos;
-	int hdrsize = sizeof(*eth);
-	struct vlan_hdr *vlh;
-	__u16 h_proto;
-	int i;
+                                             void *data_end,
+                                             struct ethhdr **ethhdr,
+                                             struct collect_vlans *vlans) {
+  struct ethhdr *eth = nh->pos;
+  int hdrsize = sizeof(*eth);
+  struct vlan_hdr *vlh;
+  __u16 h_proto;
+  int i;
 
-	/* Byte-count bounds check; check if current pointer + size of header
-	 * is after data_end.
-	 */
-	if (nh->pos + hdrsize > data_end)
-		return -1;
+  /* Byte-count bounds check; check if current pointer + size of header
+   * is after data_end.
+   */
+  if (nh->pos + hdrsize > data_end)
+    return -1;
 
-	nh->pos += hdrsize;
-	*ethhdr = eth;
-	vlh = nh->pos;
-	h_proto = eth->h_proto;
+  nh->pos += hdrsize;
+  *ethhdr = eth;
+  vlh = nh->pos;
+  h_proto = eth->h_proto;
 
-	/* Use loop unrolling to avoid the verifier restriction on loops;
-	 * support up to VLAN_MAX_DEPTH layers of VLAN encapsulation.
-	 */
-	#pragma unroll
-	for (i = 0; i < VLAN_MAX_DEPTH; i++) {
-		if (!proto_is_vlan(h_proto))
-			break;
-
-		if (vlh + 1 > data_end)
-			break;
-
-		h_proto = vlh->h_vlan_encapsulated_proto;
-		if (vlans) /* collect VLAN ids */
-			vlans->id[i] =
-				(bpf_ntohs(vlh->h_vlan_TCI) & VLAN_VID_MASK);
-
-		vlh++;
-	}
-
-	nh->pos = vlh;
-	return h_proto; /* network-byte-order */
-}
-
-static __always_inline int parse_ethhdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct ethhdr **ethhdr)
-{
-	/* Expect compiler removes the code that collects VLAN ids */
-	return parse_ethhdr_vlan(nh, data_end, ethhdr, NULL);
-}
-
-static __always_inline int parse_ip6hdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct ipv6hdr **ip6hdr)
-{
-	struct ipv6hdr *ip6h = nh->pos;
-
-	/* Pointer-arithmetic bounds check; pointer +1 points to after end of
-	 * thing being pointed to. We will be using this style in the remainder
-	 * of the tutorial.
-	 */
-	if (ip6h + 1 > data_end)
-		return -1;
-
-	nh->pos = ip6h + 1;
-	*ip6hdr = ip6h;
-
-	return ip6h->nexthdr;
-}
-
-static __always_inline int parse_iphdr(struct hdr_cursor *nh,
-				       void *data_end,
-				       struct iphdr **iphdr)
-{
-	struct iphdr *iph = nh->pos;
-	int hdrsize;
-
-	if (iph + 1 > data_end)
-		return -1;
-
-	hdrsize = iph->ihl * 4;
-	/* Sanity check packet field is valid */
-	if(hdrsize < sizeof(*iph))
-		return -1;
-
-	/* Variable-length IPv4 header, need to use byte-based arithmetic */
-	if (nh->pos + hdrsize > data_end)
-		return -1;
-
-	nh->pos += hdrsize;
-	*iphdr = iph;
-
-	return iph->protocol;
-}
-
-static __always_inline int parse_icmp6hdr(struct hdr_cursor *nh,
-					  void *data_end,
-					  struct icmp6hdr **icmp6hdr)
-{
-	struct icmp6hdr *icmp6h = nh->pos;
-
-	if (icmp6h + 1 > data_end)
-		return -1;
-
-	nh->pos   = icmp6h + 1;
-	*icmp6hdr = icmp6h;
-
-	return icmp6h->icmp6_type;
-}
-
-static __always_inline int parse_icmphdr(struct hdr_cursor *nh,
-					 void *data_end,
-					 struct icmphdr **icmphdr)
-{
-	struct icmphdr *icmph = nh->pos;
-
-	if (icmph + 1 > data_end)
-		return -1;
-
-	nh->pos  = icmph + 1;
-	*icmphdr = icmph;
-
-	return icmph->type;
-}
-
-static __always_inline int parse_icmphdr_common(struct hdr_cursor *nh,
-						void *data_end,
-						struct icmphdr_common **icmphdr)
-{
-	struct icmphdr_common *h = nh->pos;
-
-	if (h + 1 > data_end)
-		return -1;
-
-	nh->pos  = h + 1;
-	*icmphdr = h;
-
-	return h->type;
-}
-
-/*
- * parse_udphdr: parse the udp header and return the length of the udp payload
+/* Use loop unrolling to avoid the verifier restriction on loops;
+ * support up to VLAN_MAX_DEPTH layers of VLAN encapsulation.
  */
-static __always_inline int parse_udphdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct udphdr **udphdr)
-{
-	int len;
-	struct udphdr *h = nh->pos;
+#pragma unroll
+  for (i = 0; i < VLAN_MAX_DEPTH; i++) {
+    if (!proto_is_vlan(h_proto))
+      break;
 
-	if (h + 1 > data_end)
-		return -1;
+    if ((void *)(vlh + 1) > data_end)
+      break;
 
-	nh->pos  = h + 1;
-	*udphdr = h;
+    h_proto = vlh->h_vlan_encapsulated_proto;
+    if (vlans) /* collect VLAN ids */
+      vlans->id[i] = (bpf_ntohs(vlh->h_vlan_TCI) & VLAN_VID_MASK);
 
-	len = bpf_ntohs(h->len) - sizeof(struct udphdr);
-	if (len < 0)
-		return -1;
+    vlh++;
+  }
 
-	return len;
+  nh->pos = vlh;
+  return h_proto; /* network-byte-order */
 }
 
-/*
- * parse_tcphdr: parse and return the length of the tcp header
- */
-static __always_inline int parse_tcphdr(struct hdr_cursor *nh,
-					void *data_end,
-					struct tcphdr **tcphdr)
-{
-	int len;
-	struct tcphdr *h = nh->pos;
-
-	if (h + 1 > data_end)
-		return -1;
-
-	len = h->doff * 4;
-	/* Sanity check packet field is valid */
-	if(len < sizeof(*h))
-		return -1;
-
-	/* Variable-length TCP header, need to use byte-based arithmetic */
-	if (nh->pos + len > data_end)
-		return -1;
-
-	nh->pos += len;
-	*tcphdr = h;
-
-	return len;
+static __always_inline int parse_ethhdr(struct hdr_cursor *nh, void *data_end,
+                                        struct ethhdr **ethhdr) {
+  /* Expect compiler removes the code that collects VLAN ids */
+  return parse_ethhdr_vlan(nh, data_end, ethhdr, 0);
 }
 
-#endif /* __PARSING_HELPERS_H */
+static __always_inline int parse_iphdr(struct hdr_cursor *nh, void *data_end,
+                                       struct iphdr **iphdr) {
+  struct iphdr *iph = nh->pos;
+  int hdrsize;
+
+  if ((void *)(iph + 1) > data_end)
+    return -1;
+
+  hdrsize = iph->ihl * 4;
+  /* Sanity check packet field is valid */
+  if (hdrsize < sizeof(*iph))
+    return -1;
+
+  /* Variable-length IPv4 header, need to use byte-based arithmetic */
+  if (nh->pos + hdrsize > data_end)
+    return -1;
+
+  nh->pos += hdrsize;
+  *iphdr = iph;
+
+  return iph->protocol;
+}
+
+static __always_inline int parse_ip6hdr(struct hdr_cursor *nh, void *data_end,
+                                        struct ipv6hdr **ip6hdr) {
+  struct ipv6hdr *ip6h = nh->pos;
+
+  if ((void *)(ip6h + 1) > data_end)
+    return -1;
+
+  nh->pos = ip6h + 1;
+  *ip6hdr = ip6h;
+
+  return ip6h->nexthdr;
+}
+
+static __always_inline int parse_udphdr(struct hdr_cursor *nh, void *data_end,
+                                        struct udphdr **udphdr) {
+  int len = 0;
+  struct udphdr *h = nh->pos;
+
+  if ((void *)(h + 1) > data_end)
+    return -1;
+
+  nh->pos = h + 1;
+  *udphdr = h;
+
+  len = bpf_ntohs(h->len) - sizeof(struct udphdr);
+  if (len < 0)
+    return -1;
+
+  return len;
+}
+
+static __always_inline int parse_tcphdr(struct hdr_cursor *nh, void *data_end,
+                                        struct tcphdr **tcphdr) {
+  int len;
+  struct tcphdr *h = nh->pos;
+
+  if ((void *)(h + 1) > data_end)
+    return -1;
+
+  len = h->doff * 4;
+  /* Sanity check packet field is valid */
+  if (len < sizeof(*h))
+    return -1;
+
+  /* Variable-length TCP header, need to use byte-based arithmetic */
+  if (nh->pos + len > data_end)
+    return -1;
+
+  nh->pos += len;
+  *tcphdr = h;
+
+  return len;
+}
+
+static __always_inline int parse_icmphdr(struct hdr_cursor *nh, void *data_end,
+                                         struct icmphdr **icmphdr) {
+  struct icmphdr *icmph = nh->pos;
+
+  if ((void *)(icmph + 1) > data_end)
+    return -1;
+
+  nh->pos = icmph + 1;
+  *icmphdr = icmph;
+
+  return icmph->type;
+}
+
+static __always_inline int parse_icmp6hdr(struct hdr_cursor *nh, void *data_end,
+                                          struct icmp6hdr **icmp6hdr) {
+  struct icmp6hdr *icmp6h = nh->pos;
+
+  if ((void *)(icmp6h + 1) > data_end)
+    return -1;
+
+  nh->pos = icmp6h + 1;
+  *icmp6hdr = icmp6h;
+
+  return icmp6h->icmp6_type;
+}
+
+#endif /* __PARSE_HELPERS_H */
